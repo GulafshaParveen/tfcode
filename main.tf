2df3078a2f0e5bcdd3e8e2ac0d6555014093b57f
GulafshaParveen <155970442+GulafshaParveen@users.noreply.github.com>
2024-04-03 16:47:33 +0530

    Create main.tf
    Terraform file check

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+
+ required_providers {
+
+  docker = {
+
+   source = "kreuzwerker/docker"
+
+   version = "2.16.0"
+
+  }
+
+ }
+
+}
+
+
+
+provider "docker" {
+
+ host = "unix:///var/run/docker.sock"
+
+}
+
+
+
+
+
+resource "docker_image" "nginx" {
+
+ name = "nginx:latest"
+
+}
+
+
+
+
+
+resource "docker_container" "foo" {
+
+ image = docker_image.nginx.latest
+
+ name = "foo"
+
+}
